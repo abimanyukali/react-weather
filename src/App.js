050334f42df466341c165d17d01e33b459e2478c
abimanyukali <abiabimanyu350@gmail.com>
2021-09-19 03:42:43 +1000

    your awwsome message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Inputer from "./component/input";
+import axios from "axios";
+import Result from "./component/Result";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [temp, setTemp] = useState("");
+  const [description, setDescription] = useState("");
+  const [icon, setIcon] = useState("");
+
+  const findWeather = async () => {
+    const apiKey = "9181e93bea1baf5a421d06e1dcc750af";
+    const unit = "metric";
+    const url =
+      `https://api.openweathermap.org/data/2.5/weather?q=` +
+      input +
+      "&appid=" +
+      apiKey +
+      "&units=" +
+      unit;
+
+    const response = await axios.get(url);
+    console.log(response);
+    const temp = await response.data.main.temp;
+    const weatherDescription = await response.data.weather[0].description;
+    const icon = await response.data.weather[0].icon;
+    const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+    setTemp(temp);
+    setDescription(weatherDescription);
+    setIcon(imageUrl);
+    setInput ("");  
+    // console.log(temp,weatherDescription,imageUrl);
+
+    //const url = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {temp === "" ? (
+        <Inputer input={input} setInput={setInput} findWeather={findWeather} />
+      ) : (
+        <Result temp={temp} desc={description} icon={icon} setTemp={setTemp} />
+      )}
     </div>
   );
 }
---
+++ src/component/Result.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Result.css";
+import { Button } from "@material-ui/core";
+export const Result = ({ temp, icon, desc, setTemp }) => {
+  return (
+    <div className="result">
+      <div className="result__box">
+        <h1>Temp:{temp}</h1>
+        <p>{desc}</p>
+        <img src={icon} alt="logo" /> <br />
+        <Button
+          variant="contained"
+          color="default"
+          onClick={() => {
+            setTemp("");
+          }}
+        >
+          Back
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default Result;
